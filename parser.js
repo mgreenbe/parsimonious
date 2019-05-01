019b2f871c8c5599e56344bbf2f2958a08ee19d9
Matthew Greenberg <mgreenbe@outlook.com>
2019-05-01 17:15:31 -0600

    more combinators

--- parser.js
+++ parser.js
@@ -23,6 +23,10 @@ class Parser extends Function {
     });
   }
 
+  pair(p) {
+    return this.chain(v => p.map(w => [v, w]));
+  }
+
   then(p) {
     return this.chain(() => p);
   }
@@ -31,8 +35,8 @@ class Parser extends Function {
     return this.chain(v => p.map(() => v));
   }
 
-  pair(p) {
-    return this.chain(v => p.map(w => [v, w]));
+  between(p, q) {
+    return p.then(this.skip(q));
   }
 
   try() {
@@ -40,23 +44,98 @@ class Parser extends Function {
       let resp1 = this(input, state);
       return resp1.outcome === "success"
         ? resp1
-        : { ...resp1, index: state.index };
+        : { ...resp1, failedState: state };
     });
   }
 
   or(p) {
     return new Parser((input, state) => {
       let resp = this(input, state);
-      return resp.outcome === "success" || resp.index !== state.index
+      return resp.outcome === "success" ||
+        resp.failedState.index !== state.index
         ? resp
         : p(input, state);
     });
+  }
+
+  maybe() {
+    return this.chain(x => pure([x])).or(pure([]));
+  }
+
+  fallback(x) {
+    return this.or(pure(x));
+  }
+
+  atMost(max) {
+    return new Parser((input, state) => {
+      let values = [];
+      let resp = pure([])(input, state);
+      while (values.length < max) {
+        resp = this(input, state);
+        if (resp.outcome === "success") {
+          if (resp.newState.index === state.index) {
+            throw new Error(
+              "To guarantee that p.many() terminates, p must consume input."
+            );
+          }
+          values.push(resp.value);
+          state = resp.newState;
+        } else if (
+          resp.outcome === "failure" &&
+          resp.failedState.index === state.index
+        ) {
+          break;
+        } else {
+          return resp;
+        }
+      }
+      console.log(values);
+      return success(values, resp.failedState || resp.newState);
+    });
+  }
+
+  times(min, max = min) {
+    return this.atMost(max).chain(x => (x.length < min ? zero : pure(x)));
+  }
+
+  atLeast(min) {
+    return this.times(min, Infinity);
+  }
+
+  many() {
+    return this.times(0, Infinity);
+  }
+
+  sepBy(p) {
+    return this.maybe().chain(x =>
+      p
+        .then(this)
+        .many()
+        .map(xs => [...x, ...xs])
+    );
+  }
+
+  lookahead() {
+    return getState.chain(state => this.skip(setState(state)));
+  }
+
+  failsSoftly() {
+    return this.maybe().chain(x => (x.length === 0 ? pure(null) : zero));
+  }
+
+  notFollowedBy(p) {
+    return this.skip(p.failsSoftly());
   }
 
   __call__(input, state) {
     return this.parse(input, state);
   }
 }
+
+let getState = new Parser((_input, state) => success(state, state));
+
+let setState = newState =>
+  new Parser((_input, _state) => success(null, newState));
 
 let seq = (...ps) =>
   new Parser((input, state) => {
@@ -79,7 +158,11 @@ let alt = (...ps) =>
     let resp = zero(input, state);
     for (let p of ps) {
       resp = p(input, state);
-      if (resp.outcome === "success" || resp.index !== state.index) {
+      console.log(resp);
+      if (
+        resp.outcome === "success" ||
+        resp.failedState.index !== state.index
+      ) {
         return resp;
       }
     }
@@ -93,15 +176,15 @@ function pure(x) {
   });
 }
 
-let zero = new Parser((_input, state) => failure(null, state.index));
-
-let failure = (expected, index) => {
+let failure = (expected, failedState) => {
   return {
     outcome: "failure",
     expected,
-    index
+    failedState
   };
 };
+
+let zero = new Parser((_input, state) => failure(null, state));
 
 function success(value, newState) {
   return {
@@ -114,15 +197,15 @@ function success(value, newState) {
 let take = new Parser((input, state) => {
   let x = input[state.index];
   return x === undefined
-    ? failure("a token", state.index)
+    ? failure("a token", state)
     : success(x, { ...state, index: state.index + 1 });
 });
 
-let string = s =>
+let string = str =>
   new Parser((input, state) => {
-    return input.slice(state.index, state.index + s.length) === s
-      ? success(s, { ...state, index: state.index + s.length })
-      : failure(s, state.index);
+    return input.slice(state.index, state.index + str.length) === str
+      ? success(str, { ...state, index: state.index + str.length })
+      : failure(str, state);
   });
 
 let p = take
@@ -139,4 +222,4 @@ s = seq();
 t = alt(take.pair(take), zero);
 
 u = string("abc");
-console.log(u("abdef", { index: 0 }));
+console.log(string("a").sepBy(string("b"))("abababababac", { index: 0 }));
